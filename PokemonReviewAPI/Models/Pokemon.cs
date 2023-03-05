dc064c44d8e78271dbab9ff61c94216f3a9cc537
lucas-perata <contacto.perata@gmail.com>
2023-03-05 16:30:47 -0300

    Add one-to-many to models

--- PokemonReviewAPI/Models/Pokemon.cs
+++ PokemonReviewAPI/Models/Pokemon.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public string Description { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
     }
 }
