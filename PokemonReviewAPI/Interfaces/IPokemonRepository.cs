c757ba9d817764b24c19a44e2ee0300cb0489533
lucas-perata <contacto.perata@gmail.com>
2023-03-06 16:08:37 -0300

    Repository model for Pokemon

---
+++ PokemonReviewAPI/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeID); 
+    }
+}
