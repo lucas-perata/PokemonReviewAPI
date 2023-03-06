0d7bd11d86760af38304d836e486c3a7cf5b89ca
lucas-perata <contacto.perata@gmail.com>
2023-03-06 16:07:37 -0300

    Mapping helper for Pokemon Model

---
+++ PokemonReviewAPI/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PokemonReviewAPI.Dto;
+using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
