3173117ac6ef478fe0b593e5a3e3aec95772c9b5
lucas-perata <contacto.perata@gmail.com>
2023-03-05 21:02:42 -0300

    Fix to Models

--- PokemonReviewAPI/Models/Pokemon.cs
+++ PokemonReviewAPI/Models/Pokemon.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
-        public string Description { get; set; }
 
         public ICollection<Review> Reviews { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
--- PokemonReviewAPI/Models/Review.cs
+++ PokemonReviewAPI/Models/Review.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public int Rating { get; set; }
+
         public string Text { get; set; }
 
         public Reviewer Reviewer { get; set; }
