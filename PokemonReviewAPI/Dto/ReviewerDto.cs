ec693d1b340846649bc35706b2368a6a2edbc9b0
lucas-perata <contacto.perata@gmail.com>
2023-03-07 15:45:48 -0300

    REVIEWER/DTO fix for showing reviews for get request

--- PokemonReviewAPI/Dto/ReviewerDto.cs
+++ PokemonReviewAPI/Dto/ReviewerDto.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public ICollection<ReviewDto> Reviews { get; set; }
     }
 }
