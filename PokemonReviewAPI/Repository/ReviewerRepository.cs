0e5cc1fc88128a5c21e6f6b18463e8d18d0821bd
lucas-perata <contacto.perata@gmail.com>
2023-03-07 15:25:27 -0300

    REVIEWER/Add  DTO, MAP, IRepo, Repo and Controller for GET requests

---
+++ PokemonReviewAPI/Repository/ReviewerRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewAPI.Data;
+using PokemonReviewAPI.Interfaces;
+using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+
+        public ReviewerRepository(DataContext context)
+        {
+            _context = context; 
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _context.Reviewers.Where(r => r.Id == reviewerId).Include(e => e.Reviews).FirstOrDefault();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId) 
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
